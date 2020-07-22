3fc31cebe6e7213ef443277689516415e0d23ad7
roywu <roywu@webank.com>
2020-07-22 16:28:43 +0800

    Update box api && box unittest

--- wecube_plugins_itsdangerous/apps/processor/api.py
+++ wecube_plugins_itsdangerous/apps/processor/api.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+import hashlib
+import json
 import logging
 
 from talos.common import cache
@@ -19,34 +21,36 @@ Subject = resource.Subject
 
 class Box(resource.Box):
 
-    def _get_rules(self, data):
-        boxes = self.list(filters={'policy.enabled': 1, 'subject.enabled': 1})
+    def _get_rules(self, data, boxes=None):
+        boxes = boxes or self.list(filters={'policy.enabled': 1, 'subject.enabled': 1})
         rules = {}
+        hasher = hashlib.sha256()
+        hasher.update(json.dumps(data).encode('utf-8'))
+        digest = hasher.hexdigest()
         for b in boxes:
             subject_included = False
             for target in b['subject']['targets']:
-                target_included = []
-                key = 'scope/target/%s' % target['id']
+                target_included = True
+                # target with the same data is cached
+                key = 'scope/target/%s/data/%s' % (target['id'], digest)
                 cached = cache.get(key, 30)
-                if cache.validate(cached) and cached:
-                    target_included.append(True)
+                if cache.validate(cached):
+                    target_included = cached
                 else:
                     if target['enabled']:
                         if target['args_scope'] is not None:
-                            target_included.append(scope.JsonScope(target['args_scope']).is_match(data))
+                            target_included = scope.JsonScope(target['args_scope']).is_match(data)
                         else:
-                            target_included.append(True)
-                        if target_included and target['entity_scope'] is not None:
-                            target_included.append(scope.WeCMDBScope(target['entity_scope']).is_match(data['entityInstances']))
-                        else:
-                            target_included.append(True)
+                            target_included = True
+                        if target_included:
+                            if target['entity_scope'] is not None:
+                                target_included = scope.WeCMDBScope(target['entity_scope']).is_match(data['entityInstances'])
+                            else:
+                                target_included = True
                     else:
-                        target_included.append(False)
-                if False in target_included:
-                    cache.set(key, False)
-                    continue
-                else:
-                    cache.set(key, True)
+                        target_included = False
+                cache.set(key, target_included)
+                if target_included:
                     subject_included = True
                     break
             if subject_included:
@@ -64,28 +68,27 @@ class Box(resource.Box):
             rs.append(r)
         return results
 
-    def check(self, data):
-        with self.get_session() as session:
-            results = []
-            service_name = data['serviceName']
-            input_params = data['inputParams']
-            entity_instances = data['entityInstances']
-            # TODO: one or more scripts support
-            script = input_params.get('script', '') or ''
-            script_type = input_params.get('script_type', None)
-            rules = self._get_rules(data)
-            rules = self._rule_grouping(rules)
-            for key, values in rules.items():
-                if not script_type:
-                    script_type = reader.guess(script) or 'text'
-                if key == 'filter':
-                    results.extend(detector.JsonFilterDetector(data, values).check())
-                elif key == 'cli' and script_type == 'shell':
-                    results.extend(detector.BashCliDetector(script, values).check())
-                elif key == 'sql' and script_type == 'sql':
-                    results.extend(detector.SqlDetector(script, values).check())
-                elif key == 'text':
-                    results.extend(detector.LineTextDetector(script, values).check())
-                elif key == 'fulltext':
-                    results.extend(detector.FullTextDetector(script, values).check())
-            return results
+    def check(self, data, boxes=None):
+        results = []
+        service_name = data['serviceName']
+        input_params = data['inputParams']
+        entity_instances = data['entityInstances']
+        # TODO: one or more scripts support
+        script = input_params.get('script', '') or ''
+        script_type = input_params.get('script_type', None)
+        rules = self._get_rules(data, boxes=boxes)
+        rules = self._rule_grouping(rules)
+        for key, values in rules.items():
+            if not script_type:
+                script_type = reader.guess(script) or 'text'
+            if key == 'filter':
+                results.extend(detector.JsonFilterDetector(data, values).check())
+            elif key == 'cli' and script_type == 'shell':
+                results.extend(detector.BashCliDetector(script, values).check())
+            elif key == 'sql' and script_type == 'sql':
+                results.extend(detector.SqlDetector(script, values).check())
+            elif key == 'text':
+                results.extend(detector.LineTextDetector(script, values).check())
+            elif key == 'fulltext':
+                results.extend(detector.FullTextDetector(script, values).check())
+        return results
