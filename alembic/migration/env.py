20b32a9759b12e471a807c1324d0d52f75bb922d
roywu <roywu@webank.com>
2020-08-12 17:55:04 +0800

    Update alembic conf

--- alembic/migration/env.py
+++ alembic/migration/env.py
@@ -7,9 +7,8 @@ import os
 import sys
 
 from alembic import context
-from wecube_plugins_itsdangerous.db.models import Base
+from wecube_plugins_itsdangerous.db.models_manage import Base
 from sqlalchemy import engine_from_config, pool
-
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -45,8 +44,7 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True)
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -59,16 +57,12 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix='sqlalchemy.',
-        poolclass=pool.NullPool)
+    connectable = engine_from_config(config.get_section(config.config_ini_section),
+                                     prefix='sqlalchemy.',
+                                     poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
