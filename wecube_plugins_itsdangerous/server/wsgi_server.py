b70dda4d0dda57aa35e218bb7ae4bdb434c43acd
royl88 <576477085@qq.com>
2020-09-04 11:35:31 +0800

    #1 support for config item intercept

--- wecube_plugins_itsdangerous/server/wsgi_server.py
+++ wecube_plugins_itsdangerous/server/wsgi_server.py
@@ -9,21 +9,23 @@ wecube_plugins_itsdangerous.server.wsgi_server
 
 from __future__ import absolute_import
 
+import base64
 import os
 import json
 from talos.server import base
 from talos.core import utils
-# from talos.core import config
+from wecube_plugins_itsdangerous.server import conf_intercepter
+from talos.core import config
 
-# @config.intercept('db_password', 'other_password')
-# def get_password(value, origin_value):
-#     """value为上一个拦截器处理后的值（若此函数为第一个拦截器，等价于origin_value）
-#        origin_value为原始配置文件的值
-#        没有拦截的变量talos将自动使用原始值，因此定义一个拦截器是很关键的
-#        函数处理后要求必须返回一个值
-#     """
-#     # 演示使用不安全的base64，请使用你认为安全的算法进行处理
-#     return base64.b64decode(origin_value)
+
+@config.intercept('itsdangerous_db_username', 'itsdangerous_db_password', 'itsdangerous_db_hostip',
+                  'itsdangerous_db_hostport', 'itsdangerous_db_schema', 'gateway_url', 'jwt_signing_key')
+def get_env_value(value, origin_value):
+    prefix = 'ENV@'
+    if value.startswith(prefix):
+        env_name = value[len(prefix):]
+        return os.getenv(env_name, default='')
+    raise ValueError('config intercepter of get_env_value support "ENV@" only')
 
 
 def error_serializer(req, resp, exception):
