263cacb8f1eeff49dc3367637dcd67b94f37a67f
roywu <roywu@webank.com>
2020-11-02 16:06:39 +0800

    #1 bug fix

--- wecube_plugins_itsdangerous/server/wsgi_server.py
+++ wecube_plugins_itsdangerous/server/wsgi_server.py
@@ -18,17 +18,17 @@ from talos.server import base
 from talos.core import utils
 from talos.core import config
 
-from wecube_plugins_itsdangerous.common import utils
+from wecube_plugins_itsdangerous.common import utils as plugin_utils
 from wecube_plugins_itsdangerous.middlewares import auth
 
-RAS_KEY_PATH = '/certs/ras_key'
+RSA_KEY_PATH = '/certs/rsa_key'
 
 
-def decrypt_ras(secret_key, encrypt_text):
+def decrypt_rsa(secret_key, encrypt_text):
     rsakey = RSA.importKey(secret_key)
     cipher = Cipher_pkcs1_v1_5.new(rsakey)
     random_generator = Random.new().read
-    text = cipher.decrypt(utils.b64decode_key(encrypt_text), random_generator)
+    text = cipher.decrypt(plugin_utils.b64decode_key(encrypt_text), random_generator)
     return text.decode('utf-8')
 
 
@@ -41,10 +41,10 @@ def get_env_value(value, origin_value):
         env_name = value[len(prefix):]
         new_value = os.getenv(env_name, default='')
         if new_value.startswith(encrypt_prefix):
-            certs_path = RAS_KEY_PATH
+            certs_path = RSA_KEY_PATH
             if os.path.exists(certs_path) and os.path.isfile(certs_path):
                 with open(certs_path) as f:
-                    new_value = decrypt_ras(f.read(), new_value)
+                    new_value = decrypt_rsa(f.read(), new_value[len(encrypt_prefix):])
             else:
                 raise ValueError('keys with "RSA@", but rsa_key file not exists')
         return new_value
