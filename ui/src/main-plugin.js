ab5eed0076ff4734f85adec78a9fca62dc9b2452
pobu168 <244202969@qq.com>
2020-11-04 16:23:15 +0800

    Update config

--- ui/src/main-plugin.js
+++ ui/src/main-plugin.js
@@ -1,9 +1,31 @@
 import 'regenerator-runtime/runtime'
-import router from './router-plugin'
-import zhCN from './locale/i18n/zh-CN.json'
-import enUS from './locale/i18n/en-US.json'
+import router from './router'
+import 'view-design/dist/styles/iview.css'
+import './locale/i18n'
+import { validate } from '@/assets/js/validate.js'
+import { commonUtil } from '@/pages/util/common-util.js'
+import '@/assets/css/local.bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'font-awesome/css/font-awesome.css'
+import VeeValidate from '@/assets/veeValidate/VeeValidate'
+import jquery from 'jquery'
+import zhCN from '@/assets/locale/i18n/zh-CN.json'
+import enUS from '@/assets/locale/i18n/en-US.json'
+
+import PageTable from '@/pages/components/table-page/page'
+import ModalComponent from '@/pages/components/modal'
+
+window.addOptions({
+  JQ: jquery,
+  $commonUtil: commonUtil,
+  $validate: validate
+})
+
+window.component('PageTable', PageTable)
+window.component('ModalComponent', ModalComponent)
+window.use(VeeValidate)
 
 window.locale('zh-CN', zhCN)
 window.locale('en-US', enUS)
 
-window.addRoutes && window.addRoutes(router, 'cmdb')
+window.addRoutes && window.addRoutes(router, 'dangerous')
