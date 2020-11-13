fd3f7384fa72fa5794dc1c026e6ed996d07e9da6
pobu168 <244202969@qq.com>
2020-11-13 10:20:40 +0800

    Update ui config

--- ui/src/api/base-plugin.js
+++ ui/src/api/base-plugin.js
@@ -1,7 +1,7 @@
 export const pluginErrorMessage = async r => {
   const res = await r
-  if (res.statusCode.startsWith('ERR')) {
-    const errorMes = Array.isArray(res.data) ? res.data.map(_ => _.errorMessage).join('<br/>') : res.statusMessage
+  if (res.status.startsWith('ERR')) {
+    const errorMes = Array.isArray(res.data) ? res.data.map(_ => _.error).join('<br/>') : res.message
     window.vm &&
       window.vm.$Notice.error({
         title: 'Error',
