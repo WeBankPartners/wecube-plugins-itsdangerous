3cce3f2e697c475de6970b07b2a672c2289967cf
pobu168 <244202969@qq.com>
2020-11-19 11:10:05 +0800

    update server.js

--- ui/src/api/server.js
+++ ui/src/api/server.js
@@ -6,7 +6,8 @@ if (window.request) {
     post: (url, ...params) => pluginErrorMessage(window.request.post(baseURL + url, ...params)),
     get: (url, ...params) => pluginErrorMessage(window.request.get(baseURL + url, ...params)),
     delete: (url, ...params) => pluginErrorMessage(window.request.delete(baseURL + url, ...params)),
-    put: (url, ...params) => pluginErrorMessage(window.request.put(baseURL + url, ...params))
+    put: (url, ...params) => pluginErrorMessage(window.request.put(baseURL + url, ...params)),
+    patch: (url, ...params) => pluginErrorMessage(window.request.patch(baseURL + url, ...params))
   }
 }
 
