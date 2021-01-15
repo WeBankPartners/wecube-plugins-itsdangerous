773de119293dee3673e3e9cd5c802115f3430492
roywu <roywu@webank.com>
2021-01-15 15:26:30 +0800

    #32 add operation log

--- wecube_plugins_itsdangerous/middlewares/permission.py
+++ wecube_plugins_itsdangerous/middlewares/permission.py
@@ -2,16 +2,20 @@
 
 from __future__ import absolute_import
 
+import logging
 from talos.core import config
 from talos.core import utils as base_utils
 from talos.core import exceptions as base_ex
 
+LOG = logging.getLogger(__name__)
 CONF = config.CONF
 
 
 class Permission(object):
     """中间件，提供API权限校验"""
     def process_resource(self, req, resp, resource, params):
+        request_data = getattr(req, 'json', None)
+        LOG.info('[%s] "%s %s" %s', req.auth_user, req.method, req.relative_uri, request_data)
         controller_name = getattr(resource, 'name', None)
         data_permissions = base_utils.get_config(CONF, 'data_permissions', {})
         if controller_name is not None and controller_name in data_permissions:
