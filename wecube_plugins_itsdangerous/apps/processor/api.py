c65ad5929715fbe5c042119fb672559a3acddabc
Roy <576477085@qq.com>
2025-04-27 14:56:32 +0800

    restrict check result output lines

--- wecube_plugins_itsdangerous/apps/processor/api.py
+++ wecube_plugins_itsdangerous/apps/processor/api.py
@@ -388,6 +388,8 @@ class Box(resource.Box):
             table.set_cols_align(["l", "c", "l", "l", "l"])
             table.set_cols_valign(["m", "m", "m", "m", "m"])
             table.header([_("Instance Ids"), _("Line"), _("Content"), _("Message"), _('Source Script')])
+            counter = 1
+            max_counter = 100
             for ret in results:
                 associate_ids = ','.join([(inst.get('displayName', '') or '#' + str(inst.get('id', '')))
                                           for inst in associate_instances])
@@ -395,8 +397,16 @@ class Box(resource.Box):
                     associate_ids,
                     '%s-%s' % (ret['lineno'][0], ret['lineno'][1]), ret['content'], ret['message'], ret['script_name']
                 ])
+                if counter >= max_counter and len(results) > max_counter:
+                    more_lines = len(results) - max_counter
+                    table.add_row([
+                        associate_ids,
+                        '...', '%d more lines(%d total)' % (more_lines, len(results)), '...', '...'
+                    ])
+                    break
+                counter += 1
             text_output = table.draw()
-        return {'text': text_output, 'data': results}
+        return {'text': utils.truncate_for_text(text_output), 'data': results}
 
     def plugin_check(self, data):
         '''run plugin params check
@@ -451,6 +461,8 @@ class Box(resource.Box):
             table.set_cols_align(["l", "c", "l", "l", "l"])
             table.set_cols_valign(["m", "m", "m", "m", "m"])
             table.header([_("Instance Ids"), _("Line"), _("Content"), _("Message"), _('Source Script')])
+            counter = 1
+            max_counter = 100
             for associate_instances, ret in render_results:
                 associate_ids = ','.join([(inst.get('displayName', '') or '#' + str(inst.get('id', '')))
                                           for inst in associate_instances])
@@ -458,8 +470,16 @@ class Box(resource.Box):
                     associate_ids,
                     '%s-%s' % (ret['lineno'][0], ret['lineno'][1]), ret['content'], ret['message'], ret['script_name']
                 ])
+                if counter >= max_counter and len(render_results) > max_counter:
+                    more_lines = len(render_results) - max_counter
+                    table.add_row([
+                        associate_ids,
+                        '...', '%d more lines(%d total)' % (more_lines, len(render_results)), '...', '...'
+                    ])
+                    break
+                counter += 1
             text_output = table.draw()
-        return {'text': text_output, 'data': results}
+        return {'text': utils.truncate_for_text(text_output), 'data': results}
 
     def check(self, data, boxes=None, without_subject_test=False, handover_match_params=None):
         '''check script & param with boxes, return dangerous contents & rule name
